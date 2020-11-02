4abda8d4875aa69239dbeeee50cf12e493a84bab
Robbie Love <Robbie@ubiquilife.com>
2020-11-02 16:30:20 +1000

    add Customer controller with action to show customers.blade.php view and added /customers route, start to show Customer data on view in alphanumeric order

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class CustomerController extends Controller
 {
-    //
+    public function Action()
+    {
+        $customers = Customer::orderBy('name', 'asc')->get();
+//        dd($customers);
+        return view('customers', compact('customers'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// get the customers
+Route::get('customers', [CustomerController::class, 'Action']);
